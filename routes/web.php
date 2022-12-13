6446390397af9b90071f6657c52bfd9d8c0e311c
Blue89Azul <life.89.blue@gmail.com>
2022-12-13 20:04:11 +0900

    update / add delete tweet action

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,4 @@ Route::post('/tweet/create', CreateController::class)->name('tweet.create');
 
 Route::get('tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
 Route::put('tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
-
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
